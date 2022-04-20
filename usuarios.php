2c5383b7cb416f9e78d405a699e3572c2855fc86
David-sah <david.hernandez1@catolica.edu.sv>
2022-04-20 17:46:52 -0600

    Update usuarios.php

--- usuarios.php
+++ usuarios.php
@@ -29,11 +29,11 @@
                 <div class="row">
                     <div class="col-12">
                         <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                            <h4 class="mb-sm-0">Lista de Empleados</h4>
+                            <h4 class="mb-sm-0"><?= $lang["t-ListOfEmployees"] ?></h4>
                             <div class="page-title-right">
                                 <ol class="breadcrumb m-0">
                                     <li class="breadcrumb-item"><a href="javascript: void(0);">Menu</a></li>
-                                    <li class="breadcrumb-item active">Empleados</li>
+                                    <li class="breadcrumb-item active"><?= $lang["t-ListOfEmployees"] ?></li>
                                 </ol>
                             </div>
 
@@ -46,7 +46,7 @@
                     <div class="col-lg-12">
                         <div class="card">
                             <div class="card-header">
-                                <h4 class="card-title mb-0">Lista de Empleados</h4>
+                                <h4 class="card-title mb-0"><?= $lang["t-ListOfEmployees"] ?></h4>
                             </div><!-- end card header -->
 
                             <div class="card-body">
@@ -66,7 +66,7 @@
                                         <div class="col-sm">
                                             <div class="d-flex justify-content-sm-end">
                                                 <div class="search-box ms-2">
-                                                    <input type="text" class="form-control search" placeholder="Search...">
+                                                    <input type="text" class="form-control search" placeholder="<?= $lang["t-search"] ?>">
                                                     <i class="ri-search-line search-icon"></i>
                                                 </div>
                                             </div>
